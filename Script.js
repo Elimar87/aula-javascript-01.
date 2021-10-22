9deb75a4ff47ea5d424d376b553924b269b88d9a
Elimar Santos <88753613+Elimar87@users.noreply.github.com>
2021-10-22 00:06:17 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,27 @@
+// tipos de dados
+function  variavel  ( ) {
+    var  idadeDoCliente  =  20 ;
+    var  nomeDoCliente  =  'Luana Souza De Melo' ;
+    var  clienteMaiorDeIdade  =  true ;
+    var  dadosDoCliente  =  [ 'Luana' ,  '852.147.194-05' ,  21 ]
+    var  cliente  =  { nome : 'Elimar' ,  idade : 20 ,  ano : 2001 }
+    var  assuntoDaAulaDoDia18Do09 ;
+    var  assuntoDeAmanha  =  null ;
+  }
+
+
+
+
+
+function  tiposDeVariaveis  ( ) {
+    var  nome ;
+    nome  =  'Elimar' ;
+    var  idade ;
+    idade  =  20 ;
+    let  assuntoDaAulaDeHoje  =  'tipos de dados' ;
+   
+    const  cpfDoCliente  =  '055.840.971-06' ;
+    const  idDoProduto  =  05 ;
+}
+ let  estarAprendendoAaulaDeHoje  =  true ;
+console . log ( estarAprendendoAaulaDeHoje )
